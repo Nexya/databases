fd40d441c3cde4bce3ee5340d39450325c3df46b
Theres Wallinder <52582658+Nexya@users.noreply.github.com>
2023-01-23 12:54:17 +0100

    some view experimenting

--- views.sql
+++ views.sql
@@ -1,3 +1,11 @@
 /*
 TODO:   implement views
 */
+
+
+-- BasicInformation(idnr, name, login, program, branch)
+CREATE VIEW BasicInformation AS 
+SELECT Students.idnr, Students.name, Students.login, Students.program, 
+(SELECT branch FROM StudentBranches WHERE StudentBranches.student = Students.idnr)
+FROM Students;
+
