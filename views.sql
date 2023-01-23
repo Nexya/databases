eefe042dfd9a5c0af610d9851f763c91a5134ace
Theres Wallinder <52582658+Nexya@users.noreply.github.com>
2023-01-23 13:22:27 +0100

    some more view testing

--- views.sql
+++ views.sql
@@ -6,6 +6,11 @@ TODO:   implement views
 -- BasicInformation(idnr, name, login, program, branch)
 CREATE VIEW BasicInformation AS 
 SELECT Students.idnr, Students.name, Students.login, Students.program, 
-(SELECT branch FROM StudentBranches WHERE StudentBranches.student = Students.idnr)
+(SELECT branch FROM StudentBranches WHERE StudentBranches.student = Students.idnr) 
 FROM Students;
 
+-- FinishedCourses(student, course, grade, credits)
+CREATE VIEW FinishedCourses AS
+SELECT Taken.student, Taken.course, Taken.grade,
+(SELECT credits FROM Courses WHERE Taken.course = Courses.code) -- TODO: fix round error?
+FROM Taken;
