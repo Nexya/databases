77d67ee31b44191b2f21f9204013f8ff9719cbdb
Theres Wallinder <52582658+Nexya@users.noreply.github.com>
2023-01-23 12:43:47 +0100

    foreign keys added still no constraints

--- tables.sql
+++ tables.sql
@@ -25,13 +25,16 @@ CREATE TABLE Courses(
 
 CREATE TABLE LimitedCourses(
     code        TEXT PRIMARY KEY,
-    capacity    INT
+    capacity    INT,
+    FOREIGN KEY (code) REFERENCES Courses
 );
 
 CREATE TABLE StudentBranches(
     student     TEXT PRIMARY KEY,
     branch      TEXT,
-    program     TEXT
+    program     TEXT,
+    FOREIGN KEY (student) REFERENCES Students,
+    FOREIGN KEY (branch, program) REFERENCES Branches
 );
 
 CREATE TABLE Classifications(
@@ -40,39 +43,57 @@ CREATE TABLE Classifications(
 
 CREATE TABLE Classified(
     course       TEXT,
-    classifications TEXT
+    classifications TEXT,
+    PRIMARY KEY (course, classifications),
+    FOREIGN KEY (course) REFERENCES Courses,
+    FOREIGN KEY (classifications) REFERENCES Classifications
 );
 
 CREATE TABLE MandatoryProgram(
     course      TEXT,
-    program     TEXT
+    program     TEXT,
+    PRIMARY KEY (course, program),
+    FOREIGN KEY (course) REFERENCES Courses
 );
 
 CREATE TABLE MandatoryBranch(
     course      TEXT,
     branch      TEXT,
-    program     TEXT
+    program     TEXT,
+    PRIMARY KEY (course, branch, program),
+    FOREIGN KEY (branch, program) REFERENCES Branches
 );
 
 CREATE TABLE RecommendedBranch(
     course      TEXT,
     branch      TEXT,
-    program     TEXT
+    program     TEXT,
+    PRIMARY KEY (course, branch, program),
+    FOREIGN KEY (branch, program) REFERENCES Branches
 );
 
 CREATE TABLE Registered(
     student     TEXT,
-    course      TEXT
+    course      TEXT,
+    PRIMARY KEY (student, course),
+    FOREIGN KEY (student) REFERENCES Students,
+    FOREIGN KEY (course) REFERENCES Courses
 );
 
 CREATE TABLE Taken(
     student     TEXT,
     course      TEXT,
-    grade       TEXT
+    grade       TEXT,
+    PRIMARY KEY (student, course),
+    FOREIGN KEY (student) REFERENCES Students,
+    FOREIGN KEY (course) REFERENCES Courses
 );
 
 CREATE TABLE WaitingList(
     student     TEXT,
     course      TEXT,
-    position    TEXT
+    position    SERIAL,
+    PRIMARY KEY (student, course),
+    FOREIGN KEY (student) REFERENCES Students,
+    FOREIGN KEY (course) REFERENCES LimitedCourses
 );
