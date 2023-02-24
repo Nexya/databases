028706b7a5c197887d9d7e0e65acfd5b5ce0b648
Theres Wallinder <52582658+Nexya@users.noreply.github.com>
2023-02-24 13:43:56 +0100

    moved functions

--- triggers.sql
+++ triggers.sql
@@ -1,9 +1,59 @@
+CREATE VIEW CourseQueuePositions
+SELECT course, student, position AS place
+FROM WaitingList;
+
+
+
 CREATE TRIGGER checkGradeFound
 AFTER INSERT ON Registered
 FOR EACH ROW
 EXECUTE PROCEDURE haveGrade();
 
+CREATE FUNCTION gradeFound(stu TEXT,cou TEXT) RETURNS BOOLEAN AS $$
+BEGIN
+    RETURN (SELECT (SELECT grade FROM Taken WHERE Taken.course = cou
+    AND Taken.student = stu) IS NOT NULL);
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE FUNCTION haveGrade() RETURNS TRIGGER AS $$
+BEGIN
+    IF (gradeFound(NEW.student,NEW.course))
+        THEN RAISE EXCEPTION 'You already have a grade';
+    END IF;
+    RETURN NULL;
+END
+$$ LANGUAGE 'plpgsql';
+
+
+
 CREATE TRIGGER checkOverCapacity
 AFTER INSERT ON Registered
 FOR EACH ROW
 EXECUTE PROCEDURE limitedCapacity();
+
+CREATE FUNCTION isLimitedCapacityCourse(course TEXT) RETURNS BOOLEAN AS $$
+BEGIN
+    RETURN (SELECT (SELECT code FROM LimitedCourses WHERE code = course) IS NOT NULL);
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE FUNCTION limitedCapacity() RETURNS TRIGGER AS $$
+DECLARE
+    currentCourseCapacity INT := 
+        (SELECT capacity FROM LimitedCourses WHERE LimitedCourses.code = NEW.course);
+    studentRegistered INT := 
+        (SELECT (SELECT COUNT(student) FROM Registered WHERE Registered.course = NEW.course));
+BEGIN
+    IF (isLimitedCapacityCourse(NEW.course))
+        THEN 
+        IF (studentRegistered > currentCourseCapacity)
+            THEN
+            INSERT INTO WaitingList VALUES(NEW.student,NEW.course,5);
+            DELETE FROM Registered 
+            WHERE Registered.course = NEW.course AND Registered.student = NEW.student;
+        END IF;
+    END IF;
+    RETURN NULL;
+END
+$$ LANGUAGE 'plpgsql';
