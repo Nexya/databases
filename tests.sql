ba6697ba9a2f1916ced3bac6802cd30c2023db4e
Theres Wallinder <52582658+Nexya@users.noreply.github.com>
2023-03-03 19:39:21 +0100

    some progress, not much left

--- tests.sql
+++ tests.sql
@@ -14,14 +14,26 @@ INSERT INTO Registrations VALUES ('4444444444', 'CCC555');
 -- EXPECTED OUTCOME: Fail
 INSERT INTO Registrations VALUES ('4444444444','CCC111');
 
--- TEST #5: Check prerequisite.
+-- TEST #5: Check prerequisite that is not fulfilled
 -- EXPECTED OUTCOME: Fail
 INSERT INTO Registrations VALUES ('1111111111','CCC222');
 
 -- TEST #6: Unregister registered student
 -- EXPECTED OUTCOME: Pass
-DELETE FROM Registered WHERE student = '2222222222' AND course = 'CCC111';
+DELETE FROM Registrations WHERE student = '2222222222' AND course = 'CCC111';
 
--- TEST #7: Unregister student that has already been unregistered
+-- TEST #9: Unregister student that has already been unregistered
 -- EXPECTED OUTCOME: Fail
-DELETE FROM Registered WHERE student = '2222222222' AND course = 'CCC111';
+DELETE FROM Registrations WHERE student = '2222222222' AND course = 'CCC111'; -- NOT WORKING
+
+-- TEST #10: Add student to waitinglist that is already there 
+-- EXPECTED OUTCOME: Fail
+INSERT INTO Registrations VALUES ('3333333333','CCC222'); 
+
+-- TEST #11: Check prerequisite that is fulfilled
+-- EXPECTED OUTCOME: Pass
+INSERT INTO Registrations VALUES ('5555555555','CCC333');
+
+-- TEST #12: Unregister from an overfull course with a waiting list -- ADD OVERFULL 
+-- EXPECTED OUTCOME: Pass
+DELETE FROM Registrations WHERE course = 'CCC333' AND student = '2222222222';
--- triggers.sql
+++ triggers.sql
@@ -11,13 +11,14 @@ DECLARE
     checkRegistered INT;
     checkPassed INT;
     checkPrerequisite TEXT;
+    newposition INT;
 BEGIN
 
     -- has the student passed the required prerequisite? 
     checkPrerequisite := 
         ((SELECT needCourse FROM Prerequisite WHERE forCourse = NEW.course) 
         EXCEPT
-        (SELECT Taken.course FROM Taken WHERE Taken.student = NEW.student));
+        (SELECT PassedCourses.course FROM PassedCourses WHERE PassedCourses.student = NEW.student));
         IF checkPrerequisite IS NOT NULL THEN 
             RAISE EXCEPTION 'Student has not passed the required prerequisite for this course.';
         END IF;
@@ -46,20 +47,21 @@ BEGIN
     checkRegistered  := 
         (SELECT (SELECT COUNT(student) FROM registrations WHERE registrations.course = NEW.course AND status = 'registered'));
         IF checkRegistered >= checkCoursecapacity THEN
-            INSERT INTO waitinglist VALUES (NEW.student, NEW.course);
+            newposition := 
+                ((SELECT MAX(place) FROM CourseQueuePositions WHERE CourseQueuePositions.course = NEW.course) + 1);
+            INSERT INTO waitinglist VALUES (NEW.student, NEW.course, newposition);
         ELSE
             INSERT INTO registered VALUES (NEW.student, NEW.course);
         END IF;
 
+    
     RETURN NEW;
 END;
 $register$ LANGUAGE 'plpgsql';
 
-CREATE TRIGGER register INSTEAD OF INSERT ON registrations
+DROP TRIGGER IF EXISTS register ON Registrations;
+CREATE TRIGGER register INSTEAD OF INSERT ON Registrations
     FOR EACH ROW EXECUTE FUNCTION register();
-
-
-
 
 --- UNREGISTER TRIGGER
 
@@ -71,28 +73,32 @@ CREATE OR REPLACE FUNCTION unregister() RETURNS trigger AS $unregister$
     BEGIN
         -- check if student is already unregistered or not registered to begin with
         checkUnregistered := 
-            (SELECT student FROM Registrations WHERE student = OLD.student AND course = OLD.course);
-        IF checkUnregistered IS NOT NULL THEN
+            (SELECT COUNT(student) FROM Registrations WHERE student = OLD.student AND course = OLD.course);
+        IF checkUnregistered IS NULL THEN
             RAISE EXCEPTION 'Student is not registered, and therefore cannot be unregistered';
         END IF;
 
         -- delete student from registered and/or waiting list
         DELETE FROM Registered  WHERE student = OLD.student AND course = OLD.course;
-        DELETE FROM WaitingList WHERE student = OLD.student AND course = OLD.course;
+        DELETE FROM WaitingList WHERE student = OLD.student AND limitedCourse = OLD.course;
+        
 
         -- check course capacity if next student in waiting list can register, if yes do so
         courseCount := 
             (SELECT capacity FROM LimitedCourses WHERE LimitedCourses.code = OLD.course);
         numStudents := 
-            (SELECT COUNT(*) FROM Registered WHERE Registered.course = OLD.course);
+            (SELECT COUNT(student) FROM Registered WHERE Registered.course = OLD.course);
         IF numStudents < courseCount THEN 
             INSERT INTO Registered (SELECT student, course FROM WaitingList WHERE course = OLD.course AND position = 1);
-            DELETE FROM WaitingList WHERE student = OLD.student AND course = OLD.course AND position = 1);
+            DELETE FROM WaitingList WHERE position = 1;
+             -- update waiting list
+            UPDATE WaitingList SET position = position - 1 WHERE limitedCourse = OLD.course; -- TODO: add so it works correctly when middle of list is removed
         END IF;
 
-        -- update waiting list TODO: needs to loop through each position
-        UPDATE WaitingList SET position = position - 1 WHERE OLD.position < position AND OLD.limitedCourse = limitedCourse;
         RETURN NULL;
     END;
 $unregister$ LANGUAGE 'plpgsql';
 
+DROP TRIGGER IF EXISTS unregister ON Registrations;
+CREATE TRIGGER unregister INSTEAD OF DELETE OR UPDATE ON Registrations
+    FOR EACH ROW EXECUTE FUNCTION unregister();
